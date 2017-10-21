7fd619ca8edd13003cb63f10b92e2c30712bacbe
Logan Brown <logan-brown@uiowa.edu>
2017-10-20 19:51:10 -0500

    push 2

--- Project/src/Word.java
+++ Project/src/Word.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Word {
+    final private String word;
+    final private String pronunciation;
+    final private String definitions;
+    private double frequency;
+    private  double difficulty;
+    private ArrayList rules;
+
+    Word(String word, String pronunciation, String definitions){
+        this.word = word;
+        this.pronunciation = pronunciation;
+        this.definitions = definitions;
+    }
+
 }
